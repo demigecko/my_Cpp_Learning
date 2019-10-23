6383dca710122a3c4ecd4aded85929a215181085
Hsin-Ying Chiu <hsinying@gmail.com>
2019-10-22 21:18:05 -0700

    Create main.cpp

---
+++ my_Cpp_Learning/my_Cpp_Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  my_Cpp_Learning
+//
+//  Created by Hsin-Ying Chiu on 10/22/19.
+//  Copyright © 2019 Hsin-Ying Chiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
